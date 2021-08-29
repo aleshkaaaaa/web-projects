60481bf3834f6a2f07cefe3f46776268a96c007f
aleshkaaaaa <aleksey_2909@mail.ru>
2021-08-29 17:30:50 +0300

    fix

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import { diffDates, diffToHtml } from "./datecalc.js"; // 1
-import { formatError } from "./utils.js"; // 2
+import { diffDates, diffToHtml } from "./datecalc.js";
+import { formatError } from "./utils.js";
 
 const dateCalcForm = document.getElementById("datecalc");
 const dateCalcResult = document.getElementById("datecalc__result");
@@ -7,7 +7,7 @@ const dateCalcResult = document.getElementById("datecalc__result");
 dateCalcForm.addEventListener("submit", handleCalcDates);
 
 function handleCalcDates(event) {
-    
+
     dateCalcResult.innerHTML = "";
     event.preventDefault();
 
@@ -15,8 +15,8 @@ function handleCalcDates(event) {
     firstDate = firstDate.value, secondDate = secondDate.value;
 
     if (firstDate && secondDate) {
-        const diff = diffDates(firstDate, secondDate); // 3
-        dateCalcResult.innerHTML = diffToHtml(diff); // 4
+        const diff = diffDates(firstDate, secondDate);
+        dateCalcResult.innerHTML = diffToHtml(diff);
     }
-    else dateCalcResult.innerHTML = formatError("Для расчета промежутка необходимо заполнить оба поля"); // 5
+    else dateCalcResult.innerHTML = formatError("Для расчета промежутка необходимо заполнить оба поля");
 }
